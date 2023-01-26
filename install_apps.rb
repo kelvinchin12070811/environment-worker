79f51118580cb028917768b2cd08505889c20c3f
Kelvin Chin <chinkaiwen@yahoo.com>
2023-01-26 16:01:39 +0800

    moved apps list to its own folder

--- install_apps.rb
+++ install_apps.rb
@@ -3,38 +3,9 @@
 
 require 'open-uri'
 require 'json'
+require 'fileutils'
 
-APPS = [
-  { name: 'Mozilla Firefox', id: 'Mozilla.Firefox' },
-  { name: 'Visual Studio Code', id: 'Microsoft.VisualStudioCode' },
-  { name: 'KeePassXC', id: 'KeePassXCTeam.KeePassXC' },
-  { name: 'Dropbox', id: 'Dropbox.Dropbox' },
-  { name: 'Evernote', id: '9WZDNCRFJ3MB' },
-  { name: 'Spotify', id: '9NCBCSZSJRSB' },
-  { name: 'Discord', id: 'Discord.Discord' },
-  { name: 'Git', id: 'Git.Git' },
-  { name: 'Foxit PDF Reader', id: 'Foxit.FoxitReader' },
-  { name: 'Visual Studio Community 2022', id: 'Microsoft.VisualStudio.2022.Community' },
-  { name: 'Dolby Access', id: '9N0866FS04W8' },
-  { name: 'Character Map UWP', id: '9WZDNCRDXF41' },
-  { name: 'EarTrumpet', id: '9NBLGGH516XP' },
-  { name: 'TranslucentTB', id: '9PF4KZ2VN4W9' },
-  { name: 'Microsoft PowerToys', id: 'Microsoft.PowerToys' },
-  { name: 'NVM for Windows', id: 'CoreyButler.NVMforWindows' },
-  { name: 'Steam', id: 'Valve.Steam' },
-  { name: 'PowerShell', id: 'Microsoft.PowerShell' },
-  { name: 'Windows Terminal', id: '9N0DX20HK701' },
-  { name: 'Telegram Desktop', id: 'Telegram.TelegramDesktop' },
-  { name: 'NordVPN', id: 'NordVPN.NordVPN' },
-  { name: 'NordLocker', id: 'Nord.NordLocker' },
-  { name: 'AutoHotKey', id: 'AutoHotKey' },
-  { name: 'NanaZip', id: '9N8G7TSCL18R' },
-  { name: 'Cloudflare WARP', id: 'Cloudflare.Warp' },
-  { name: 'IntelliJ IDEA Community Edition', id: 'JetBrains.IntelliJIDEA.Community' },
-  { name: 'Shotcut', id: 'Meltytech.Shotcut' },
-  { name: 'Krita', id: 'KDE.Krita' },
-  { name: 'KritaShellExtension', id: 'KDE.KritaShellExtension' }
-].freeze
+require_relative './winget_apps_list'
 
 puts "Stage 1: Winget -- #{APPS.length} apps to install."
 
@@ -52,14 +23,14 @@ puts ''
 puts 'Stage 2: Custom installations'
 puts ''
 
-TEMP_WORKSPACE_NAME = 'temp-worksapce'
-Dir.rmdir(TEMP_WORKSPACE_NAME) if Dir.exist?(TEMP_WORKSPACE_NAME)
+TEMP_WORKSPACE_NAME = 'temp-workspace'
+FileUtils.rm_r(TEMP_WORKSPACE_NAME) if Dir.exist?(TEMP_WORKSPACE_NAME)
 Dir.mkdir(TEMP_WORKSPACE_NAME)
 Dir.chdir(TEMP_WORKSPACE_NAME)
 
-custom_plans = Dir.children('custom-installations')
+custom_plans = Dir.children('../custom-installations')
 custom_plans.each_with_index do |plan, index|
-  print "#{index} of #{custom_plans.length}: "
+  print "#{index + 1} of #{custom_plans.length}: "
   require_relative "./custom-installations/#{plan}"
   puts ''
 end
---
+++ winget_apps_list.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+APPS = [
+  { name: 'Mozilla Firefox', id: 'Mozilla.Firefox' },
+  { name: 'Visual Studio Code', id: 'Microsoft.VisualStudioCode' },
+  { name: 'KeePassXC', id: 'KeePassXCTeam.KeePassXC' },
+  { name: 'Dropbox', id: 'Dropbox.Dropbox' },
+  { name: 'Evernote', id: '9WZDNCRFJ3MB' },
+  { name: 'Spotify', id: '9NCBCSZSJRSB' },
+  { name: 'Discord', id: 'Discord.Discord' },
+  { name: 'Git', id: 'Git.Git' },
+  { name: 'Foxit PDF Reader', id: 'Foxit.FoxitReader' },
+  { name: 'Visual Studio Community 2022', id: 'Microsoft.VisualStudio.2022.Community' },
+  { name: 'Dolby Access', id: '9N0866FS04W8' },
+  { name: 'Character Map UWP', id: '9WZDNCRDXF41' },
+  { name: 'EarTrumpet', id: '9NBLGGH516XP' },
+  { name: 'TranslucentTB', id: '9PF4KZ2VN4W9' },
+  { name: 'Microsoft PowerToys', id: 'Microsoft.PowerToys' },
+  { name: 'NVM for Windows', id: 'CoreyButler.NVMforWindows' },
+  { name: 'Steam', id: 'Valve.Steam' },
+  { name: 'PowerShell', id: 'Microsoft.PowerShell' },
+  { name: 'Windows Terminal', id: '9N0DX20HK701' },
+  { name: 'Telegram Desktop', id: 'Telegram.TelegramDesktop' },
+  { name: 'NordVPN', id: 'NordVPN.NordVPN' },
+  { name: 'NordLocker', id: 'Nord.NordLocker' },
+  { name: 'AutoHotKey', id: 'AutoHotKey' },
+  { name: 'NanaZip', id: '9N8G7TSCL18R' },
+  { name: 'Cloudflare WARP', id: 'Cloudflare.Warp' },
+  { name: 'IntelliJ IDEA Community Edition', id: 'JetBrains.IntelliJIDEA.Community' },
+  { name: 'Shotcut', id: 'Meltytech.Shotcut' },
+  { name: 'Krita', id: 'KDE.Krita' },
+  { name: 'KritaShellExtension', id: 'KDE.KritaShellExtension' }
+].freeze
