78e3c08712047dd4bec650976b18e5fb21f398fb
Kelvin Chin <chinkaiwen@yahoo.com>
2023-05-30 21:47:33 +0800

    Fixed betterdiscord install script

--- custom-installations/betterdiscord.rb
+++ custom-installations/betterdiscord.rb
@@ -7,7 +7,7 @@ puts 'Installing Better Discord...'
 better_discord_releases = URI.open('https://api.github.com/repos/BetterDiscord/Installer/releases/latest').read
 better_discord_assets = JSON.parse(better_discord_releases)['assets']
 better_discord_assets.each do |asset|
-  next if asset['content_type'] != 'application/x-ms-dos-executable'
+  next if asset['name'] != 'BetterDiscord-Windows.exe'
 
   puts "Downloading installer (#{asset['size']} Bytes)..."
   system "curl -OL #{asset['browser_download_url']}"
